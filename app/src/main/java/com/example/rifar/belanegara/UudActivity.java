a1cbcb2fcf8b185b9af4e868666afbf3cd15a9c6
rifardity <rifardity@gmail.com>
2017-09-08 21:44:21 +0700

    Add Search

--- app/src/main/java/com/example/rifar/belanegara/UudActivity.java
+++ app/src/main/java/com/example/rifar/belanegara/UudActivity.java
@@ -2,6 +2,7 @@ package com.example.rifar.belanegara;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.rifar.belanegara.architecture.UudViewModel;
 import com.example.rifar.belanegara.module.BindingApplicator;
@@ -14,6 +15,9 @@ public class UudActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         vm = new UudViewModel();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         applicator = new BindingApplicator();
         applicator.applyBinding(this, R.layout.uud_view, vm);
     }
