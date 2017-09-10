fa71cdde54218dfbb10feccb3ad7e99e3d31c30b
rifardity <rifardity@gmail.com>
2017-09-10 15:56:04 +0700

    Add Search

--- app/src/main/java/com/example/rifar/belanegara/UudActivity.java
+++ app/src/main/java/com/example/rifar/belanegara/UudActivity.java
@@ -1,8 +1,17 @@
 package com.example.rifar.belanegara;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.rifar.belanegara.architecture.UudViewModel;
 import com.example.rifar.belanegara.module.BindingApplicator;
@@ -10,16 +19,59 @@ import com.example.rifar.belanegara.module.BindingApplicator;
 public class UudActivity extends AppCompatActivity {
     private UudViewModel vm;
     private BindingApplicator applicator;
+    private Toolbar mToolbar;
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         vm = new UudViewModel();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         applicator = new BindingApplicator();
         applicator.applyBinding(this, R.layout.uud_view, vm);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Undang Undang");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(isSearchOpened) {
+            handleMenuSearch();
+            return;
+        }
+        super.onBackPressed();
     }
 
     @Override
@@ -27,6 +79,46 @@ public class UudActivity extends AppCompatActivity {
         super.onDestroy();
         applicator.removeBinding();
     }
+
+
+    private void doSearch() {
+
+    }
+
+
+    protected void handleMenuSearch(){
+        ActionBar action = getSupportActionBar();
+
+        if(isSearchOpened){
+            action.setDisplayShowCustomEnabled(false);
+            action.setDisplayShowTitleEnabled(true);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(mToolbar.getWindowToken(), 0);
+            mSearchAction.setIcon(getDrawable(R.drawable.ic_open_search));
+            isSearchOpened = false;
+
+        } else {
+            action.setDisplayShowCustomEnabled(true);
+            action.setCustomView(R.layout.search_toolbar);
+            action.setDisplayShowTitleEnabled(false);
+            edtSearch = (EditText)action.getCustomView().findViewById(R.id.edtSearch);
+
+            edtSearch.setOnEditorActionListener((v, actionId, event) -> {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    doSearch();
+                    return true;
+                }
+                return false;
+            });
+
+            edtSearch.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSearch, InputMethodManager.SHOW_IMPLICIT);
+            mSearchAction.setIcon(getDrawable(R.drawable.ic_close_search));
+            isSearchOpened = true;
+        }
+    }
+
 
 }
 
