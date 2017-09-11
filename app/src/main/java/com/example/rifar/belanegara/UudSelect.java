95ff9bc30d5245e142368e8d516c8e0ec900fef6
rifardity <rifardity@gmail.com>
2017-09-11 22:06:17 +0700

    Add Page UUD Select

--- app/src/main/java/com/example/rifar/belanegara/UudSelect.java
+++ app/src/main/java/com/example/rifar/belanegara/UudSelect.java
@@ -2,6 +2,9 @@ package com.example.rifar.belanegara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class UudSelect extends AppCompatActivity {
@@ -9,5 +12,26 @@ public class UudSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uud_select);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar_select);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_uudselect, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_close) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
