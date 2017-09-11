fdbc918f732bc59653c0bd840f5d08bec81fc16b
rifardity <rifardity@gmail.com>
2017-09-11 09:18:21 +0700

    Add Page UUD Select

---
+++ app/src/main/java/com/example/rifar/belanegara/UudSelect.java
@@ -0,0 +1,13 @@
+package com.example.rifar.belanegara;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class UudSelect extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uud_select);
+    }
+}
